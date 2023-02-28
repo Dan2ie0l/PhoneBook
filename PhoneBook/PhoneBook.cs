3e7d3e8df492899912b868729ff19230d0c62088
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2023-02-28 15:09:29 +0400

    adding validation

--- PhoneBook/PhoneBook.cs
+++ PhoneBook/PhoneBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,35 @@ namespace PhoneBook
 {
     public class PhoneBook
     {
-       public string Name { get; set; }
-       public string Surname { get; set; }
-       public string PhoneNumber { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Separator { get; set; }
         public PhoneBook() {
-          
-        }  
+
+        }
+
+        public string Validation() {
+            string message = "Line " + this.Id ;
+
+            if (this.PhoneNumber.Length != 9)
+            {
+                message += " phone number should be 9 digits. ";
+
+            }
+            if (this.Separator != "-" && this.Separator != ":" )
+            {
+                message += " separator should be `:` or `-` ";
+            }
+           
+            return message;
+            }
+
+        public override string ToString()
+        {
+            return this.Name + " " + this.Surname + " " + this.Separator + " " + this.PhoneNumber;
+        }
 
     }
 }
--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,22 +1,46 @@
-﻿Console.Write("File Path:");
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+
+Console.Write("File Path:");
 string file =  Console.ReadLine().ToString();
 //string file = @"C:\Users\danie\source\repos\PhoneBook\PhoneBook\PhoneBook\phonebook.txt";
 
 string[] lines = File.ReadAllLines(file);
 
-List<string> names = new List<string>();
-List<string> surnames = new List<string>();
-List<string> numbers = new List<string>();
-List<string> phonebook = new List<string>();
-
+List<PhoneBook.PhoneBook> Phonebook = new();
+int i = 1;
 foreach (string line in lines)
 {
-    phonebook.Add(line);
-    Console.WriteLine(line);
+    PhoneBook.PhoneBook a = new PhoneBook.PhoneBook();
     string [] arr = line.Split(" ");
-    names.Add(arr[0]);
-    surnames.Add(arr[1]);
-    numbers.Add(arr[2]);
-    Console.WriteLine();
+    if (arr.Length == 4)
+    {
+        a.Id = i;
+        a.Name = arr[0];
+        a.Surname = arr[1];
+        a.Separator = arr[2];
+        a.PhoneNumber = arr[3];
+    }
+    else
+    {
+        a.Id = i;
+        a.Name = arr[0];
+        a.Separator = arr[1];
+        a.PhoneNumber = arr[2];
+    }
+    Phonebook.Add(a); 
+    i++;
+
 }
 
+/*Console.Write("Please choose an ordering to sort: “Ascending” or “Descending:");
+string order = Console.ReadLine().ToString();
+
+Console.Write("Please choose criteria: “Name”, “Surname” or “PhoneNumberCode:");
+string criteria = Console.ReadLine().ToString();
+*/
+foreach (var item in Phonebook)
+{
+    Console.WriteLine(item.ToString() );
+    Console.WriteLine(item.Validation());
+}
