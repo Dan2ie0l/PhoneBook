7be0999a4a3e14b9bd91ee46908d5c3070189672
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2023-02-28 16:57:41 +0400

    sorting

--- PhoneBook/PhoneBook.cs
+++ PhoneBook/PhoneBook.cs
@@ -9,30 +9,37 @@ namespace PhoneBook
 {
     public class PhoneBook
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string PhoneNumber { get; set; }
         public string Separator { get; set; }
-        public PhoneBook() {
+        public PhoneBook()
+        {
 
         }
 
-        public string Validation() {
-            string message = "Line " + this.Id ;
+        public static void Move( List<PhoneBook> list, int oldIndex, int newIndex)
+        {
+            PhoneBook item = list[oldIndex];
+            list.RemoveAt(oldIndex);
+            list.Insert(newIndex, item);
+        }
+        public string Validation()
+        {
+            string message = " ";
 
             if (this.PhoneNumber.Length != 9)
             {
                 message += " phone number should be 9 digits. ";
 
             }
-            if (this.Separator != "-" && this.Separator != ":" )
+            if (this.Separator != "-" && this.Separator != ":")
             {
                 message += " separator should be `:` or `-` ";
             }
-           
+
             return message;
-            }
+        }
 
         public override string ToString()
         {
--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,21 +1,24 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 Console.Write("File Path:");
-string file =  Console.ReadLine().ToString();
+string file = Console.ReadLine().ToString();
 //string file = @"C:\Users\danie\source\repos\PhoneBook\PhoneBook\PhoneBook\phonebook.txt";
-
+string validationMessage = "";
 string[] lines = File.ReadAllLines(file);
 
 List<PhoneBook.PhoneBook> Phonebook = new();
-int i = 1;
+
+
 foreach (string line in lines)
 {
     PhoneBook.PhoneBook a = new PhoneBook.PhoneBook();
-    string [] arr = line.Split(" ");
+    string[] arr = line.Split(" ");
+
     if (arr.Length == 4)
     {
-        a.Id = i;
         a.Name = arr[0];
         a.Surname = arr[1];
         a.Separator = arr[2];
@@ -23,24 +26,117 @@ foreach (string line in lines)
     }
     else
     {
-        a.Id = i;
         a.Name = arr[0];
+        a.Surname = " ";
         a.Separator = arr[1];
         a.PhoneNumber = arr[2];
     }
-    Phonebook.Add(a); 
-    i++;
+    Phonebook.Add(a);
 
 }
 
-/*Console.Write("Please choose an ordering to sort: “Ascending” or “Descending:");
+Console.Write("Please choose an ordering to sort: “Ascending” or “Descending:");
 string order = Console.ReadLine().ToString();
 
 Console.Write("Please choose criteria: “Name”, “Surname” or “PhoneNumberCode:");
 string criteria = Console.ReadLine().ToString();
-*/
-foreach (var item in Phonebook)
+
+
+
+
+
+
+var sort = from a in Phonebook
+           orderby a.Name
+           descending
+           select a;
+
+if (order == "Descending")
 {
-    Console.WriteLine(item.ToString() );
-    Console.WriteLine(item.Validation());
+    if (criteria == "Name")
+    {
+        sort = from a in Phonebook
+               orderby a.Name
+               descending
+               select a;
+
+    }
+    else if (criteria == "Surname")
+    {
+        sort = from a in Phonebook
+               orderby a.Surname
+               descending
+               select a;
+
+    }
+    else if (criteria == "PhoneNumberCode")
+    {
+        sort = from a in Phonebook
+               orderby a.PhoneNumber.Substring(0, 3)
+               descending
+               select a;
+
+    }
 }
+else if (order == "Ascending")
+{
+    if (criteria == "Name")
+    {
+        sort = from a in Phonebook
+               orderby a.Name
+               ascending
+               select a;
+
+    }
+    else if (criteria == "Surname")
+    {
+        sort = from a in Phonebook
+               orderby a.Surname
+               ascending
+               select a;
+    }
+    else if (criteria == "PhoneNumberCode")
+    {
+        sort = from a in Phonebook
+               orderby a.PhoneNumber.Substring(0, 3)
+               ascending
+               select a;
+
+    }
+}
+/*if(criteria == "Surname")
+{
+    var newlist = sort.ToList();
+    PhoneBook.PhoneBook.Move(newlist, 1, newlist.Count-1);
+
+    foreach (var name in newlist)
+    {
+        Console.WriteLine(name);
+    }
+}
+else
+{
+
+    foreach (var name in sort)
+    {
+        Console.WriteLine(name);
+    }
+}*/
+foreach (var name in sort)
+{
+    Console.WriteLine(name);
+}
+
+int i = 1;
+foreach (var item in sort)
+{
+    validationMessage += "Line " + i + ": " + item.Validation() + "\n";
+    i++;
+}
+Console.WriteLine();
+Console.WriteLine(validationMessage);
+
+{
+
+}
+
